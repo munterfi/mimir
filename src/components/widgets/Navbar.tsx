ae7f90377e0fd64b2e15f7bb891e99177670688e
Michael Brunner <brunner@cadwork.swiss>
2023-08-27 13:20:51 +0200

    load logo in header

--- src/components/widgets/Navbar.tsx
+++ src/components/widgets/Navbar.tsx
@@ -1,11 +1,13 @@
 import {Header} from "./Navbar.styles.ts";
+import headerImage from "../../assets/rocket.svg";
 
 function Navbar() {
-  return (
-    <div>
-      <Header>Navbar</Header>
-    </div>
-  );
+    return (
+        <div>
+            <Header>Navbar
+                <img src={headerImage} alt="headerImage"/></Header>
+        </div>
+    );
 }
 
 export default Navbar;
